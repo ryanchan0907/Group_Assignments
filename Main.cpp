3993a69bb8f29c3435fab43d532993ba2ec5f8cd
Paul Viola <Paul.viola1992@gmail.com>
2018-02-10 16:22:39 -0600

    Checked for mem leaks and added comments

--- Main.cpp
+++ Main.cpp
@@ -1,7 +1,11 @@
+// Author: Paul Viola, Bhavya Merchant, Jie Chen
+// Group Assignment 1
+
 #include <iostream>
 
 using namespace std;
 
+//Function declarations
 void fillArr(int* arrToFill, int arrSize);
 void printArr(int* arrToPrint, int arrSize);
 int dotProduct(int* arrOne, int* arrTwo, int arrSize);
@@ -9,6 +13,8 @@ void alternateArr(int* firstArr, int* secondArr, int arrSize);
 void negatives(int* arrToChange, int arrSize);
 
 int main() {
+
+	//Captures array size from user and creates 2 dynamic arrays. Size must be > 0
 	int arrSize;
 	do {
 		cout << "Enter the size of your arrays: ";
@@ -18,6 +24,8 @@ int main() {
 	int *arrOne = new int[arrSize];
 	int *arrTwo = new int[arrSize];
 
+
+	//fill arrays with ints from the user
 	cout << "Now filling up array one.\n";
 	fillArr(arrOne, arrSize);
 	cout << "----------------------------" << endl;;
@@ -41,6 +49,7 @@ int main() {
 	return 0;
 }
 
+//function fills an array with ints provided by the user
 void fillArr(int* arrToFill, int arrSize) {
 	for (int i = 0; i < arrSize; i++) {
 		cout << "Enter a number for the array: ";
@@ -49,6 +58,7 @@ void fillArr(int* arrToFill, int arrSize) {
 	}
 }
 
+//prints a specified array
 void printArr(int* arrToPrint, int arrSize) {
 	cout << "[";
 	for (int i = 0; i < arrSize; i++) {
@@ -60,6 +70,7 @@ void printArr(int* arrToPrint, int arrSize) {
 	cout << "]\n";
 }
 
+//calculates and returns the dot product of two arrays
 int dotProduct(int* arrOne, int* arrTwo, int arrSize) {
 	int product=0;
 	for (int i = 0; i < arrSize; i++) {
@@ -68,19 +79,24 @@ int dotProduct(int* arrOne, int* arrTwo, int arrSize) {
 	return product;
 }
 
+//creates an array of alternating elements and prints it
 void alternateArr(int* firstArr, int* secondArr, int arrSize) {
+	//creates a local array of twice the normal length
 	int* altArr = new int[arrSize*2];
-	for (int altArrIndex = 0, originalIndex = 0; altArrIndex < arrSize*2; i+=2) {
+	for (int altArrIndex = 0, originalIndex = 0; altArrIndex < arrSize*2; altArrIndex+=2) {
 	altArr[altArrIndex] = firstArr[originalIndex];
 	altArr[altArrIndex+1] = secondArr[originalIndex];
 	originalIndex++;
 	}
 	
+	//prints the local array and frees up memory before function ends
 	printArr(altArr, arrSize * 2);
 	delete[] altArr;
 	return;
 }
 
+
+//changes array to negative and prints it
 void negatives(int* arrToChange, int arrSize) {
 	for (int i = 0; i < arrSize; i++) {
 		arrToChange[i] = -1 * arrToChange[i];
